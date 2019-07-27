3b3523c1467f6af533277ddafd4be4e85fc4b8a5
npcenthusiasm <npcenthusiasm@outlook.com>
2019-07-28 03:12:07 +0800

    新增刪除、復原、css樣式

--- todoList.js
+++ todoList.js
@@ -1,48 +1,93 @@
 var inputText =document.querySelector('.inputText');
 var addBtn = document.querySelector('.addBtn');
 var deleteAllBtn =document.querySelector('.deleteAllBtn');
+var recoverBtn =document.querySelector('.recoverBtn');
 var todoList =document.querySelector('.todoList');
+
 var dataList = JSON.parse(localStorage.getItem('data')) ||[];
-
-update(dataList);
-
-
-//add
+var recoverData =JSON.parse(localStorage.getItem('recoverData')) ||[];
+//監聽
+todoList.addEventListener('click',deleteAndEdit);
 addBtn.addEventListener('click',addData);
+deleteAllBtn.addEventListener('click',deleteAll);
+recoverBtn.addEventListener('click',recover)
+
+updateView(dataList);
+
+//editText.addEventListener('blur',edit);
+//新增一筆紀錄
 function addData(){
     if(inputText.value != ''){
         dataList.push(inputText.value);
+        inputText.value = '';
+        updateView(dataList);
     }
-    //console.log();  
-    update(dataList);
 }
 
-function saveLocalStroage(data){
-    str = JSON.stringify(data);
-    localStorage.setItem('data',str);
-}
-function readLocalStroage(){
-    str = JSON.parse(data);
-    dataList =localStorage.getItem('data');
-    update(dataList);
-}
 
-//update
-function update(dataList){
-    str='';
+//更新畫面 + 儲存紀錄
+function updateView(dataList){
+    var str='';
     for(var i=0 ; i<dataList.length;i++)
     {
-        str +='<li data-num='+i+'>'+dataList[i]+'<button>刪除</button></li>';
-        
+        str +='<li><input data-num='+i+' type="text" value='+dataList[i]+'><button data-num='+i+' class="deleteBtn">刪除</button></li>';    
     }
+    console.log(dataList);
     todoList.innerHTML=str;
     saveLocalStroage(dataList);
 }
 
-//deleteAll
-deleteAllBtn.addEventListener('click',deleteAll);
-function deleteAll(dataList){
-    str='';
-    todoList.innerHTML=str;
+function saveLocalStroage(data){
+    localStorage.setItem('data',JSON.stringify(data));
+}
+
+function saveRecoverData(data){
+    
+    localStorage.setItem('recoverData',JSON.stringify(data));
+}
+
+//刪除所有紀錄
+function deleteAll(){
+    if(dataList[0]==null){
+        return;
+    }
+    var deleteData =dataList.splice(0,dataList.length)
+    recoverData = recoverData.concat(deleteData);//合併array
+    saveRecoverData(recoverData);
+    updateView(dataList);
+}
+
+//刪除 or 編輯一筆紀錄
+function deleteAndEdit(e){
+    if(e.target.nodeName=='BUTTON'){
+        var  num= e.target.dataset.num;
+        var deleteData =dataList.splice(num,1);//刪除資料，並把刪掉的資料存起來
+        recoverData = recoverData.concat(deleteData);//合併array
+        saveRecoverData(recoverData);
+        updateView(dataList);
+    }
+    if(e.target.nodeName=='INPUT'){
+        e.target.addEventListener('blur',edit);
+    }
+}
+
+function edit(e){
+    var num =e.target.dataset.num;
+    dataList.splice(num,1,e.target.value)
+    e.target.removeEventListener('blur',edit);
+    updateView(dataList);
+}
+
+function recover(){
+    if(recoverData[0]==null){
+        return;
+    }
+    var lastIndex =recoverData.length-1;
+    var lastData =recoverData[lastIndex];
+    recoverData.splice(lastIndex,1)
+    saveRecoverData(recoverData);
+    dataList.push(lastData);
+    saveLocalStroage(dataList);
+    updateView(dataList);
     
 }
